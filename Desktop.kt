bcf92dc2f55722a0c0f620e9f8cf0a15d8cd1715
trebronbluebird-bit <trebronbluebird@gmail.com>
2026-05-22 10:38:48 +0300

    Create Desktop.kt with simple Kotlin code

---
+++ Desktop.kt
@@ -0,0 +1,12 @@
+fun main() {
+    println("Welcome to Desktop Application")
+    
+    val userName = "User"
+    val appVersion = "1.0"
+    
+    println("Hello, $userName!")
+    println("App Version: $appVersion")
+    
+    val numbers = listOf(1, 2, 3, 4, 5)
+    println("Numbers: $numbers")
+}
